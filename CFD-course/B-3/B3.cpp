ec69baf1b9319869d3451af37d8c067cf2bd4082
lzh <lizihao323@outlook.com>
2024-11-21 20:00:45 +0900

    B-3

--- CFD-course/B-3/B3.cpp
+++ CFD-course/B-3/B3.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <math.h>
+void initialize(CALC_POINTS* points, int n_x, int n_y, double l_x, double l_y);
+void SetMesh();
+void Solve();
 
 const int dt = 1;  // time step
 const int N_step = 100;
@@ -15,13 +18,26 @@ typedef struct
     double V[2]; //convection velocity
     double* u; // varibles [DOF]
     double** x; // position [DOF][2]
-    double ALPHA = 1.0;
+    double ALPHA = 1.0; // diffusion coefficience
 }CALC_POINTS;
 
 typedef struct{
     double** mat;
     double*  rhs;
 }LIN_SYS;
+
+
+
+int main() {
+    CALC_POINTS points;
+    int n_x, n_y;
+    double l_x, l_y;
+    initialize(&points, n_x, n_y, l_x, l_y);
+    Solve();
+    return 0;
+}
+
+
 
 void initialize(CALC_POINTS* points, int n_x, int n_y, double l_x, double l_y)
 {
@@ -34,60 +50,35 @@ void initialize(CALC_POINTS* points, int n_x, int n_y, double l_x, double l_y)
     points->ALPHA   =   1.0;
     points->V[0]    =   1.0;
     points->V[1]    =   1.0;
-
     int DOF = n_x * n_y;
-
     points->x   =   (double**)malloc(DOF * sizeof(double*));
     for (int i=0;i<DOF;i++)
     {
         points->x[i]    =   (double*)malloc(2 * sizeof(double));
     }
-
-    // for(int i=0;i<Nc;i++)
-    //     {
-    //         if(i==5||i==6) 
-    //             cell[i].flag = 1;  // point 6 or 7
-    //         else 
-    //             cell[i].flag = 0;  // boundary
-    //         cell[i].U[0] = 0;   // u
-    //         cell[i].U[1] = 0;   // v
-    //     }
-};
-
-void SetMesh()
-{
-
-};
-
-void SolveImplicit()
-{
-    //dudu/dxdx
-    for (int time=0;time<It;time++)
+    points->u = (double*)malloc(DOF*sizeof(double));
+    for (int j = 0; j < n_y; j++)
     {
-            for(int i=0;i<4;i++)
+        for(int i =0; i < n_x; i++)
+        {
+            int idx = j* n_y + i;
+            points->x[idx][0] = i * points->dx;
+            points->x[idx][1] = j * points->dy;
+            points->u[idx]  = 0.0;
+        }
+    }
+};
+
+void Solve()
+{
+    for (int it=0; it<N_step; it++)
+    {
+        for(int i=0;i<4;i++)
+        {            
+            for(int j=0;j<3;j++)
             {
-                
-                for(int j=0;j<3;j++)
-                {
-                    if(cell[Nx*j+i].flag != 0)
-                    {
-                        double u = cell[Nx*j+i].U[0];
-                        double v = cell[Nx*j+i].U[1];
-                        
-                    }
-                }
 
             }
+        }
     }
-
 };
-
-
-
-int main()
-{
-    initialsolve();
-    SetMesh();
-    SolveImplicit();
-    return 0;
-}
