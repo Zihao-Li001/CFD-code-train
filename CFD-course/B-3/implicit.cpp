014fb5c519d2893080b5ae4c5dd42c7a9c427c15
lzh <lizihao323@outlook.com>
2024-11-25 19:21:10 +0900

    set_bc

--- CFD-course/B-3/implicit.cpp
+++ CFD-course/B-3/implicit.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-const int dt = 1;  // time step
-const int N_step = 100;
+const int n_step = 100;
+const int n_x = 4;
+const int n_y = 3;
+const double l_x =1.0;
+const double l_y=1.0;
+const double alpha = 1.0;
+double v[2]={1.0, 1.0};
+const double dt = 1.0;
+const double SOURCE_TERM = 1.0;
 
 typedef struct
-{   
-    int* flag;
+{
     int N_X;
     int N_Y;
     double l_x,l_y; // length of x, y
@@ -14,7 +20,8 @@ typedef struct
     double dy;  // cell size in Y direction  
     double ALPHA; // diffusion coefficience   
 
-    double** V; //convection velocity
+    int* flag={0};
+    double** V; //convection velocity [DOF][2]
     double* u; // varibles [DOF]
     double** x; // position [DOF][2]
 }CALC_POINTS;
@@ -48,6 +55,7 @@ void memory_allocation(
         ls->mat[i]=(double*)malloc(2 * sizeof(double));
     }
 }
+
 void memory_free(
         CALC_POINTS* points, 
         LIN_SYS* ls,
@@ -72,14 +80,98 @@ void memory_free(
     free(ls->mat);
 }
 
-void initialize(
+int index(
+    int i,
+    int j,
+    int n_x
+)
+{
+    return(n_x*j + i);
+}
+
+void set_calc_points(
         CALC_POINTS* points, 
         int n_x, 
         int n_y, 
         double l_x, 
-        double l_y, 
+        double l_y)
+{
+    points->N_X = n_x;
+    points->N_Y = n_y;
+    points->l_x =  l_x;
+    points->l_y =  l_y;
+    points->dx  =   l_x / (n_x-1);
+    points->dy  =   l_y / (n_y-1);
+    for (int j = 0; j < n_y; j++)
+    {
+        for(int i =0; i < n_x; i++)
+        {
+            int idx = index(i,j,n_x);
+            points->x[idx][0] = i * points->dx;
+            points->x[idx][1] = j * points->dy;
+        }
+    }
+};
+
+void set_matrix(
+        CALC_POINTS* points, 
+        LIN_SYS* ls,
+        double dt
+        ){
+    int n_x = points->N_X;
+    int n_y = points->N_Y;
+    double dx = points->dx;
+    double dy = points->dy;
+    double alpha = points->ALPHA;
+    double conv_x[n_x*n_y];
+    double conv_y[n_x*n_y];
+    for (int j = 1; j < n_y-1; j++)
+    {
+        for(int i =1; i < n_x-1; i++)
+        {
+            int idx = index(i,j,n_x);
+            conv_x[idx] = points->V[idx][0];
+            conv_y[idx] = points->V[idx][1];
+        }
+    }
+    
+    for(int j=1; j<n_y-1; j++){
+        for (int i=1; i<n_x-1; i++){
+            int k = index(i,j,n_x);
+            ls->mat[k][k-n_x] = -( conv_y[k]/dy + alpha / (dy*dy) ) * dt;
+            ls->mat[k][k-1  ] = -( conv_y[k]/dy + alpha / (dy*dy) ) * dt;
+            ls->mat[k][k    ] = 1 + ( conv_y[k]/dy + alpha / (dy*dy) + conv_y[k]/dy + alpha / (dy*dy) ) * dt;
+            ls->mat[k][k+1  ] = - alpha * dt / (dx*dx);
+            ls->mat[k][k+n_x] = - alpha * dt / (dy*dy);
+
+            ls->rhs[k] = points->u[k] + SOURCE_TERM * dt;
+        }
+    }
+}
+
+// initiallization 
+void initialize(
+        CALC_POINTS* points, 
+        int n_x, 
+        int n_y, 
         double alpha, 
-        double V[2]);
+        double v[2]
+        )
+{
+    points->ALPHA   =   alpha;
+
+    for (int j = 1; j < n_y-1; j++)
+    {
+        for(int i =1; i < n_x-1; i++)
+        {
+            int idx = index(i,j,n_x);
+            points->u[idx]    = 1.0;
+            points->V[idx][0] = v[0];
+            points->V[idx][1] = v[1];
+            points->flag[idx] = 1;
+        }
+    }
+};
 
 int main(
         int argc,
@@ -87,51 +179,12 @@ int main(
 ) {
     CALC_POINTS points;
     LIN_SYS     ls;
-    int n_x, n_y;
-    double l_x, l_y;
-    double alpha;
-    double V[2];
     memory_allocation(&points,&ls,n_x*n_y);
-    initialize(&points, n_x, n_y, l_x, l_y, alpha, V);
-
-
+    set_calc_points(&points, n_x, n_y, l_x, l_y);
+    initialize(&points, n_x, n_y, alpha, v);
+    for(int n=0; n < n_step; n++){
+        set_matrix(&points, &ls, dt);
+    };
     memory_free(&points,&ls,n_x*n_y);
     return 0;
 }
-
-// initiallization of cell
-void initialize(CALC_POINTS* points, int n_x, int n_y, double l_x, double l_y,double alpha, double v[2])
-{
-    points->N_X = n_x;
-    points->N_Y = n_y;
-    points->l_x =  l_x;
-    points->l_y =  l_y;
-    points->dx  =   l_x / (n_x);
-    points->dy  =   l_y / (n_y);
-    points->ALPHA   =   alpha;
-
-    for (int j = 0; j < n_y; j++)
-    {
-        for(int i =0; i < n_x; i++)
-        {
-            int idx = j* n_y + i;
-            points->x[idx][0] = i * points->dx;
-            points->x[idx][1] = j * points->dy;
-            points->u[idx]  = 0.0;
-            points->V[idx][0] = v[0];
-            points->V[idx][1] = v[1];
-            // bottom boundary
-            if(i==0)
-                points->flag[idx] = 0;
-            // top boundary
-            else if(i==(n_x-1))
-                points->flag[idx] = 0;
-        }
-        // left boundary
-        if(j==0)
-            points->flag = 0;
-        //right boundary
-        else if(j==(n_y-1))
-            points->flag = 0;
-    }
-};
